a6b69e54a58dcfd23f219751399e66b46928177d
maryanne-rose <rosemary.njeri@student.moringaschool.com>
2022-09-04 12:01:06 +0300

    commit tree 26edcc5b738294f90af59b9703f0637393eaf0a2

--- lib/song.rb
+++ lib/song.rb
@@ -9,24 +9,30 @@ class Song
   end
 
   def self.create_table
-    sql =  <<-SQL
-      CREATE TABLE IF NOT EXISTS songs (
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS songs(
         id INTEGER PRIMARY KEY,
         name TEXT,
         album TEXT
-        )
-        SQL
+      )
+    SQL
+
     DB[:conn].execute(sql)
   end
 
   def save
     sql = <<-SQL
-      INSERT INTO songs (name, album)
-      VALUES (?, ?)
+    INSERT INTO songs(name,album) VALUES (?,?)
     SQL
+    DB[:conn].execute(sql,self.name,self.album)
 
-    DB[:conn].execute(sql, self.name, self.album)
+    self.id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
+    self
+  end
 
+  def self.create(name:,album:)
+    song = Song.new(name:name,album:album)
+    song.save
   end
 
 end
